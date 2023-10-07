4f7ca0c0673fefd68b29b82f8b17d08bbbb52c5a
Mark Bryant <markb2482@gmail.com>
2023-10-07 22:23:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Trivia Questions
+// The first goal in team history was scored by?
+// Introduced prior to the 2nd season in team, who is the teams mascot?
+// Who was the first head coach in Seattle Kraken franchise history?
+// Which player scored the first "hat trick" in Seattle Kraken franchise history?
+// Who was the first Kraken player to win the Calder Memorial Trophy (Rookie of the year) in team history?
